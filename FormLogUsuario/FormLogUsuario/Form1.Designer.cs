81c053c835fbfa74c0480f01baee1886dc23dc92
fquinteros <zero.cx.one@hotmail.com>
2018-04-10 19:29:48 -0400

    Comit de prueba

---
+++ FormLogUsuario/FormLogUsuario/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace FormLogUsuario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProbarConexion = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.dgvUsuario = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRut = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDv = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtContrasenia = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTipo = new System.Windows.Forms.TextBox();
+            this.txtRutIdentificar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtRutEliminar = new System.Windows.Forms.TextBox();
+            this.txtDvEliminar = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuario)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnProbarConexion
+            // 
+            this.btnProbarConexion.Location = new System.Drawing.Point(26, 12);
+            this.btnProbarConexion.Name = "btnProbarConexion";
+            this.btnProbarConexion.Size = new System.Drawing.Size(75, 23);
+            this.btnProbarConexion.TabIndex = 0;
+            this.btnProbarConexion.Text = "Conectar";
+            this.btnProbarConexion.UseVisualStyleBackColor = true;
+            this.btnProbarConexion.Click += new System.EventHandler(this.btnProbarConexion_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(121, 12);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 1;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // dgvUsuario
+            // 
+            this.dgvUsuario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuario.Location = new System.Drawing.Point(26, 67);
+            this.dgvUsuario.Name = "dgvUsuario";
+            this.dgvUsuario.Size = new System.Drawing.Size(453, 150);
+            this.dgvUsuario.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(26, 389);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Rut:";
+            // 
+            // txtRut
+            // 
+            this.txtRut.Location = new System.Drawing.Point(93, 289);
+            this.txtRut.Name = "txtRut";
+            this.txtRut.Size = new System.Drawing.Size(120, 20);
+            this.txtRut.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "-";
+            // 
+            // txtDv
+            // 
+            this.txtDv.Location = new System.Drawing.Point(235, 289);
+            this.txtDv.Name = "txtDv";
+            this.txtDv.Size = new System.Drawing.Size(38, 20);
+            this.txtDv.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 327);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Contraseña:";
+            // 
+            // txtContrasenia
+            // 
+            this.txtContrasenia.Location = new System.Drawing.Point(93, 324);
+            this.txtContrasenia.Name = "txtContrasenia";
+            this.txtContrasenia.Size = new System.Drawing.Size(147, 20);
+            this.txtContrasenia.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 356);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Tipo:";
+            // 
+            // txtTipo
+            // 
+            this.txtTipo.Location = new System.Drawing.Point(93, 356);
+            this.txtTipo.Name = "txtTipo";
+            this.txtTipo.Size = new System.Drawing.Size(147, 20);
+            this.txtTipo.TabIndex = 11;
+            // 
+            // txtRutIdentificar
+            // 
+            this.txtRutIdentificar.Location = new System.Drawing.Point(98, 430);
+            this.txtRutIdentificar.Name = "txtRutIdentificar";
+            this.txtRutIdentificar.Size = new System.Drawing.Size(120, 20);
+            this.txtRutIdentificar.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 433);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Buscar por rut:";
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(26, 456);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 14;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 491);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Eliminar por rut:";
+            // 
+            // txtRutEliminar
+            // 
+            this.txtRutEliminar.Location = new System.Drawing.Point(101, 491);
+            this.txtRutEliminar.Name = "txtRutEliminar";
+            this.txtRutEliminar.Size = new System.Drawing.Size(120, 20);
+            this.txtRutEliminar.TabIndex = 16;
+            // 
+            // txtDvEliminar
+            // 
+            this.txtDvEliminar.Location = new System.Drawing.Point(246, 491);
+            this.txtDvEliminar.Name = "txtDvEliminar";
+            this.txtDvEliminar.Size = new System.Drawing.Size(38, 20);
+            this.txtDvEliminar.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(230, 494);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "-";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(303, 489);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 19;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 539);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtDvEliminar);
+            this.Controls.Add(this.txtRutEliminar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtRutIdentificar);
+            this.Controls.Add(this.txtTipo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtContrasenia);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDv);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRut);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgvUsuario);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnProbarConexion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuario)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProbarConexion;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.DataGridView dgvUsuario;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRut;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDv;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtContrasenia;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.TextBox txtRutIdentificar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtRutEliminar;
+        private System.Windows.Forms.TextBox txtDvEliminar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ FormLogUsuario/FormLogUsuario/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//hacemos uso de oracle
+using System.Data.OracleClient;
+
+namespace FormLogUsuario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Establecemos la conexion con la base de datos
+        OracleConnection ora = new OracleConnection("DATA SOURCE = orcl; USER ID= prueba; PASSWORD=123;");
+        private void btnProbarConexion_Click(object sender, EventArgs e)
+        {
+            //Este boton permite comprobar si se puede establecer la conexion con la DB
+            try
+            {
+                ora.Open();
+                MessageBox.Show("Conectado");
+                ora.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al conectar");               
+            }
+
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            //Alternativa 1 haciendo procedimiento en el metodo
+
+            /*ora.Open();
+            string sql = "select * from usuario";
+            OracleCommand cmd = new OracleCommand(sql, ora);
+            cmd.CommandType = System.Data.CommandType.Text;
+
+            OracleDataAdapter adaptador = new OracleDataAdapter();
+            adaptador.SelectCommand = cmd;
+            DataTable dsRetorno = new DataTable();
+            adaptador.Fill(dsRetorno);
+            dgvUsuario.DataSource = dsRetorno;
+
+            ora.Close();*/
+
+            //Alternativa 2 haciendo el procedimiento en la DB
+
+            try
+            {
+                ora.Open();
+                OracleCommand cmd = new OracleCommand("selecionarusuario", ora);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add("registros", OracleType.Cursor).Direction = ParameterDirection.Output;
+
+                OracleDataAdapter adaptador = new OracleDataAdapter();
+                adaptador.SelectCommand = cmd;
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+                dgvUsuario.DataSource = tabla;
+
+                ora.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al cargar los datos");
+            }
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ora.Open();
+                OracleCommand cmd = new OracleCommand("crear", ora);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add("rutX", OracleType.VarChar).Value = txtRut.Text;
+                cmd.Parameters.Add("dvX", OracleType.VarChar).Value = txtDv.Text;
+                cmd.Parameters.Add("cont", OracleType.VarChar).Value = txtContrasenia.Text;
+                cmd.Parameters.Add("tip", OracleType.VarChar).Value = txtTipo.Text;
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Usuario creado");
+                ora.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudo crear el usuario");
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ora.Open();
+                OracleCommand cmd = new OracleCommand("actualizar",ora);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add("rutU", OracleType.VarChar).Value = txtRutIdentificar.Text;
+                cmd.Parameters.Add("dvU",OracleType.VarChar).Value = txtDv.Text;
+                cmd.Parameters.Add("contU", OracleType.VarChar).Value = txtContrasenia.Text;
+                cmd.Parameters.Add("tipU",OracleType.VarChar).Value = txtTipo.Text;
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Usuario actualizado");
+                ora.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            ora.Open();
+            OracleCommand cmd = new OracleCommand("eliminar", ora);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add("rutU",OracleType.VarChar).Value = txtRutEliminar.Text;
+            cmd.Parameters.Add("dvU", OracleType.VarChar).Value = txtDvEliminar.Text;
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Usuario eliminado");
+            ora.Close();
+        }
+    }
+}
